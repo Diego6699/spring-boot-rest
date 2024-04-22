11c0b66f88f9b344df4cfa76baeb17796cacad5f
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-04-22 11:58:16 -0300

    user

---
+++ src/main/java/tech/diego6699/rest/entities/User.java
@@ -0,0 +1,39 @@
+package tech.diego6699.rest.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    public User() {
+    }
+
+    public User(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
